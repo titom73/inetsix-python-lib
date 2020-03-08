367c50e7346b48628de65e487ded38aee0d583cc
Thomas Grimonet <tgrimonet@arista.com>
2020-03-08 22:20:39 +0100

    Pep8 fixes

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -8,11 +8,11 @@ FILENAME_SRC = 'Book1.xlsx'
 
 
 def test_download_http_valide(url=URL_OK,
-                              local_filename=FILENAME_DL, 
+                              local_filename=FILENAME_DL,
                               filename_src=FILENAME_SRC):
     """
     Test Behavior with existing file
-    
+
     Parameters
     ----------
     url : string, optional
@@ -21,16 +21,16 @@ def test_download_http_valide(url=URL_OK,
         Filename used locally in rename test, by default FILENAME_SRC
     filename_src : string, optional
         Filename downloaded with URL, by default FILENAME_DL
-    """    
+    """
     print('* Use case: DL file with no rename')
-    function_result =  inetsix.tools.download_http(remote_file=url)
+    function_result = inetsix.tools.download_http(remote_file=url)
     assert function_result == filename_src,\
         'Filename returned is not correct: '+str(function_result)
     assert os.path.isfile(function_result) is True,\
         'File not found locally. Seems file '+str(function_result)+' has not been downloaded'
 
     print('* Use case: DL file with rename')
-    function_result =  inetsix.tools.download_http(remote_file=url,
+    function_result = inetsix.tools.download_http(remote_file=url,
                                                    local_file=local_filename)
     assert function_result == FILENAME_DL,\
         'Filename returned is not correct: '+str(function_result)
@@ -43,6 +43,6 @@ def test_download_http_404(url=URL_404):
     Test Behavior with 404 error code.
     """
     print('* Use case: DL file with 404 error')
-    function_result =  inetsix.tools.download_http(remote_file=url)
+    function_result = inetsix.tools.download_http(remote_file=url)
     assert function_result is None,\
         'Filename returned is not correct: '+str(function_result)
